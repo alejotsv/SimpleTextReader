2605372de7420f61e6723a8f415dc0f9ecd9c7d8
Alejandro Salas <asalas.wd@outlook.com>
2022-12-01 16:22:05 -0500

    Test reader

--- src/com/java21days/SimpleTextReader.java
+++ src/com/java21days/SimpleTextReader.java
@@ -1,8 +1,23 @@
 package com.java21days;
 
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+
 public class SimpleTextReader {
     SimpleTextReader(){
-        System.out.println("I'm reading a file");
+        String fileName = getClass().getResource("test.txt").getPath();
+        System.out.println(fileName);
+        try (FileReader file = new FileReader(fileName)){
+            System.out.println((char) file.read());
+            System.out.println((char) file.read());
+            System.out.println((char) file.read());
+            System.out.println((char) file.read());
+        } catch (FileNotFoundException e){
+            System.out.println("Exception: " + e.getMessage());
+        } catch (IOException e){
+            System.out.println("Exception: " + e.getMessage());
+        }
     }
 
 }
